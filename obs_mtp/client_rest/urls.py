116acc5d44e65421733bee3d01d87ce3f2181908
Agamenonrs <agamenonrsrj@gmail.com>
2019-02-09 20:18:19 -0200

    Incluido tela para iniciar o processo e contador de registros.

--- obs_mtp/client_rest/urls.py
+++ obs_mtp/client_rest/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('buscar_dados/', views.buscar_dados,  name='buscar_dados'),
+    path('form_iniciar/', views.form_iniciar,  name='form_iniciar'),
+    path('exibe_ultimo_registro/', views.exibe_ultimo_registro,  name='exibe_ultimo_registro'),
 ]
--- obs_mtp/client_rest/views.py
+++ obs_mtp/client_rest/views.py
@@ -4,12 +4,13 @@ from pymongo import MongoClient
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.http import Http404
+from bson.json_util import dumps
 
 import json
 import requests
 import time
 import pprint
-from bson.json_util import dumps
+import sys
 
 
 # from django.http import HttpResponse
@@ -19,46 +20,57 @@ client = MongoClient('mongodb://mongodb:27017')
 #Definine o database a ser utilizado
 db = client.obs_mtp
 
-def buscar_dados(request):
+def form_iniciar(request):
+	return render(request, 'iniciar_busca.html')
+
+def exibe_ultimo_registro(request):
 	
 	total = str(db.registro_gps.count())
 	data = dumps(db.registro_gps.find_one()) 
 	ultimo_registro = json.loads(data)
-	print('===================== ' + str(ultimo_registro['REGISTRO']))
-	#iniciar_processo()
-	return render(request, 'sucesso.html', {'total': total, 'ultimo_registro' : ultimo_registro['REGISTRO'][1] })
+	data_atualizacao = str(ultimo_registro['DATA_ATUALIZACAO'])
+	print('===================== ' + str( data_atualizacao))
+	return render(request, 'sucesso.html', {'total': total, 'ultimo_registro' : data_atualizacao })
 
 
-def client2(request):
-	print('=====================')
-	#url = 'http://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/obterPosicoesDaLinha/688'
-	url = 'http://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/obterTodasPosicoes'
-	resultado = ''
-	response = requests.get(url)
-	#Verifica se o retorno esta ok
-	if response.status_code == 200:
-		resultado = 'URL ATIVA'
-		rdata = response.text
-		data = json.loads(rdata)
-
-		#Passa a informacao para registro
-		registro = {'REGISTRO' : data['DATA']}
-		#Adiciona o atributo DATA_ATUALIZACAO ao json
-		registro['REGISTRO'].append( {'DATA_ATUALIZACAO' : datetime.now().strftime('%m-%d-%Y %H:%M:%S') } )
-
-		#Define a collection a ser utilizada(se nao existir cria)
-		collection = db.registro_gps
-		collection.insert_one(registro)
-	#iniciar_processo()
-	return HttpResponse(rdata)
+def buscar_dados(request):
+	iniciar_processo()
+	total = str(db.registro_gps.count())
+	data = dumps(db.registro_gps.find_one()) 
+	ultimo_registro = json.loads(data)
+	data_atualizacao = str(ultimo_registro['DATA_ATUALIZACAO'])
+	print('===================== ' + str( data_atualizacao))
+	return render(request, 'sucesso.html', {'total': total, 'ultimo_registro' : data_atualizacao })
 
 def index(request):
 	return render(request, 'index.html')
 
 def gravaRegistro():
-	print ('TIME EXECUTOU')
+	try:
+		print ('Processo executando...')
+		#url = 'http://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/obterPosicoesDaLinha/688'
+		url = 'http://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/obterTodasPosicoes'
+		resultado = ''
+		response = requests.get(url)
+		#Verifica se o retorno esta ok
+		if response.status_code == 200:
+			resultado = 'URL ATIVA'
+			rdata = response.text
+			data = json.loads(rdata)
+
+			#Passa a informacao para registro
+			registro = {'REGISTRO' : data['DATA'],'DATA_ATUALIZACAO' : datetime.now().strftime('%d-%m-%Y %H:%M:%S')}
+			#Adiciona o atributo DATA_ATUALIZACAO ao json
+			#Define a collection a ser utilizada(se nao existir cria)
+			collection = db.registro_gps
+			collection.insert_one(registro)
+	except Exception as e:
+		collection = db.erro_log
+		erro = {'data' :  str(datetime.now().strftime('%d-%m-%Y %H:%M:%S')), 'mensagem_erro' : str(e)}
+		collection.insert_one(erro)
+
 
 def iniciar_processo():
 	scheduler = BackgroundScheduler()
-	scheduler.add_job(gravaRegistro, 'interval', seconds=10)
+	scheduler.add_job(gravaRegistro, 'interval', minutes=2)
 	scheduler.start()
